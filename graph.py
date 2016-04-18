6699b5602315c9f45ded92ae3748e1f0ebed3e11
Tuomas <Tuomas@Tuomas-PC>
2016-04-18 17:16:18 +0300

    Bar graphs using rectangles and coordinate labels

--- graph.py
+++ graph.py
@@ -12,7 +12,6 @@ class qpen(QtGui.QWidget):
         super().__init__()
         
         self.initUI()
-        self.gridsize=20
         self.xgridsize=50
         self.ygridsize=10
         self.leftmargin=50
@@ -58,8 +57,6 @@ class qpen(QtGui.QWidget):
         self.somedata=Data()
         somedata=self.somedata
         type=somedata.load(self.path)
-        #print(self.path)
-        #print(somedata)
         
         qp = QtGui.QPainter()
         qp.begin(self)
@@ -74,7 +71,6 @@ class qpen(QtGui.QWidget):
         else:
             print("poor file")
         
-
         qp.end()
         
     def drawLines(self, qp, data):
@@ -85,15 +81,15 @@ class qpen(QtGui.QWidget):
             lines.append(data.get_data(x).get_data())
         
         time=lines[0]
-        
         line=data.get_data(1).get_data()
-        
         avg=data.get_data(1).get_avg()
         
+        #Draw line graph from X and Y coordinates
         for y in range(1,data.get_length()):
             color=QtGui.QColor(randint(0,255),randint(0,255),randint(0,255))
             pen = QtGui.QPen(color, 2, QtCore.Qt.SolidLine)
             qp.setPen(pen)
+            
             for x in range(0,len(time)-1):
                 x1=time[x]*self.xgridsize+self.leftmargin+self.xoffset
                 y1=-lines[y][x]+self.height()-self.lowmargin+self.yoffset
@@ -105,65 +101,82 @@ class qpen(QtGui.QWidget):
         
         thickness=2
         
-        pen = QtGui.QPen(QtCore.Qt.red, thickness, QtCore.Qt.SolidLine)
+        pen = QtGui.QPen(QtCore.Qt.black, thickness, QtCore.Qt.SolidLine)
         qp.setPen(pen)
         
         time=data.get_data(0).get_data()
         line=data.get_data(1).get_data()
-        #line2=data.get_data(2).get_data()
+        
         avg=data.get_data(1).get_avg()
         
+        
+        #Draw bars
         for x in range(0,len(time)):
             x1=x*self.xgridsize+self.leftmargin+self.xgridsize/2+self.xoffset
             y1=self.height()-self.lowmargin-thickness/2+self.yoffset
-            y2=-line[x]+self.height()-self.lowmargin+thickness/2+self.yoffset
-                
-            print(line[x])
-            qp.drawLine(x1,y1,x1,y2)
-        
             
+            qp.fillRect(x1-self.xgridsize/2+5,y1,self.xgridsize-10,-line[x],QtCore.Qt.green)
+        
+        
     def drawGrid(self, qp, data, type):
         
-        time=data.get_data(0).get_data()
+        time=data.get_data(0)
+        line=data.get_data(1)
         
-        gridsize=self.gridsize
-        xgridsize=self.xgridsize
-        ygridsize=self.ygridsize
         pen = QtGui.QPen(QtCore.Qt.black, 1, QtCore.Qt.DotLine)
         qp.setPen(pen)
         
-        #grid lines
-        #40 - 1
-        #20 - 2
-        #10 - 4
+        xgridsize=self.xgridsize
+        ygridsize=self.ygridsize
         
-        width=self.width()
-        height=self.height()
-        
-        startx=self.width()-self.width()+self.leftmargin
         starty=self.height()-self.lowmargin
         
-        yrange=int(self.yoffset/ygridsize)+int(self.height()/self.ygridsize)
-        xrange=int(-self.xoffset/xgridsize)+int(self.width()/self.xgridsize)
+        #drawing range
+        ymin=int(self.yoffset/ygridsize)
+        xmin=int(-self.xoffset/xgridsize)+1
+        ymax=int(self.yoffset/ygridsize)+int(self.height()/self.ygridsize)
+        xmax=int(-self.xoffset/xgridsize)+int(self.width()/self.xgridsize)
         
-        #LINES
+        
+        ##LINES##
         #horizontal
-        for y in range(int(self.yoffset/ygridsize),yrange):
-            qp.drawLine(startx,starty-y*ygridsize+self.yoffset,self.width(),self.height()-self.lowmargin-y*ygridsize+self.yoffset)
+        for y in range(int(self.yoffset/ygridsize),ymax):
+            qp.drawLine(self.leftmargin,starty-y*ygridsize+self.yoffset,self.width(),self.height()-self.lowmargin-y*ygridsize+self.yoffset)
         
         #vertical
-        for x in range(int(-self.xoffset/xgridsize)+1,xrange):
-            qp.drawLine(startx+x*xgridsize+self.xoffset,starty,startx+x*xgridsize+self.xoffset,0)
+        for x in range(xmin,xmax):
+            xpos=self.leftmargin+x*xgridsize+self.xoffset
+            qp.drawLine(xpos,starty,xpos,0)
         
-        #TEXT
+        ##TEXT##
         #horizontal
-        for x in range(int(-self.xoffset/xgridsize)+1,xrange):
-            qp.drawText(startx+x*xgridsize+self.xoffset,starty+20,str(x))
-            
+        if type=='LINE':
+            for x in range(xmin-1,xmax):
+                xpos=self.leftmargin+x*xgridsize+self.xoffset
+                qp.drawText(xpos,starty+20,str(x))
+                
+        if type=='BAR':
+            for x in range(xmin-1,len(time.get_data())):
+                xpos=self.leftmargin+x*xgridsize+self.xoffset
+                qp.drawText(xpos,starty+20,str(time.get_data()[x]))
+        
         #vertical
-        for y in range(int(self.yoffset/ygridsize),yrange):
-            qp.drawText(20,starty-y*ygridsize+self.yoffset,str(y*ygridsize))
+        for y in range(ymin,ymax):
+            qp.drawText(20,starty-y*ygridsize+self.yoffset+5,str(y*ygridsize))
             
+        ##TITLES##
+        #horizontal
+        qp.drawText(self.width()/2,self.height()-5,str(time.get_name()))
+        
+        #vertical
+        qp.translate(15,self.height()/2)
+        qp.rotate(-90)
+        qp.drawText(0,0,str(line.get_name()))
+        qp.rotate(90)
+        qp.translate(-15,-self.height()/2)
+        
+        
+        
             
             
 '''      
